de7f8f78bd7ceb08fb7f643dae3c5d10d60ae342
Chirag Gohil <tech.chiraggohil@gmail.com>
2025-07-13 14:47:52 +0530

    Basic Home page and some nextjs configs

---
+++ components/Hero/index.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import classes from './styles.module.css';
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image 
+          src="/images/site/hero-image.png"
+          alt="An image showing a person"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+      <h1>Welcome to My Next.js Blog App</h1>
+      <p>Blog about various topics related to web development.</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+    
+    </>
+  );
+}
+
+export default HomePage;
